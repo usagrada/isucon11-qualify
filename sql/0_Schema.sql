d28ab3caef4edfb85b4210b5faf7913b1283724b
usagrada <usagrada.kind@gmail.com>
2023-02-26 21:11:37 +0900

    Create index to isu_uuid and user_id in table isu

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -12,7 +12,9 @@ CREATE TABLE `isu` (
   `jia_user_id` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-  PRIMARY KEY(`id`)
+  PRIMARY KEY(`id`),
+  INDEX idx_isu_uuid (`jia_isu_uuid`),
+  INDEX idx_user_uuid (`jia_user_uuid`),
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 CREATE TABLE `isu_condition` (
